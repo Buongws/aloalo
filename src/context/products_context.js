66b0675026c092b9ff499225699d8dbcb9815dec
Buong <clonedopdz@gmail.com>
2022-09-29 21:11:30 +0700

    29/9

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,7 +1,7 @@
-import axios from 'axios'
-import React, { useContext, useEffect, useReducer } from 'react'
-import reducer from '../reducers/products_reducer'
-import { products_url as url } from '../utils/constants'
+import axios from "axios";
+import React, { useContext, useEffect, useReducer, useState } from "react";
+import reducer from "../reducers/products_reducer";
+import { products_url as url } from "../utils/constants";
 import {
   SIDEBAR_OPEN,
   SIDEBAR_CLOSE,
@@ -11,20 +11,36 @@ import {
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
   GET_SINGLE_PRODUCT_ERROR,
-} from '../actions'
+} from "../actions";
 
-const initialState = {}
+const initialState = {
+  isSiderbarOpen: false,
+};
 
-const ProductsContext = React.createContext()
+const ProductsContext = React.createContext();
 
 export const ProductsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const openSiderBar = () => {
+    dispatch({ type: SIDEBAR_OPEN });
+  };
+
+  const closeSideBar = () => {
+    dispatch({ type: SIDEBAR_CLOSE });
+  };
+
+  useEffect(() => {
+    openSiderBar();
+  }, []);
+
   return (
-    <ProductsContext.Provider value='products context'>
+    <ProductsContext.Provider value={{ ...state, openSiderBar, closeSideBar }}>
       {children}
     </ProductsContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useProductsContext = () => {
-  return useContext(ProductsContext)
-}
+  return useContext(ProductsContext);
+};
